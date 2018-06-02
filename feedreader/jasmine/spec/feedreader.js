789c8a127a4495c3113494ffb20a2677b70ff520
csenge <csengeweb@gmail.com>
2018-06-02 23:35:22 +0200

    added URL,name defined - BUG defined name

--- feedreader/jasmine/spec/feedreader.js
+++ feedreader/jasmine/spec/feedreader.js
@@ -31,12 +31,20 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('defined URL', function() {
+            expect(URL).toBeDefined();
+            expect(URL.length).not.toBe(0);      
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it ('defined name', function(){
+            expect(name).toBeDefined();
+            expect(name.length).not.toBe(0);
+        });
+
     });
 
 
